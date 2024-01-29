665081a1275e3384dd946272c24cbfef8d7b9c2b
puj44 <pujan007mm@gmail.com>
2024-01-30 03:17:36 +0530

    save form

--- src/routes/CreateForm.jsx
+++ src/routes/CreateForm.jsx
@@ -3,21 +3,20 @@ import { FormContext } from '../Contexts';
 import { useParams } from 'react-router-dom';
 import FieldModal from '../Components/FieldModal';
 import { toast } from 'react-toastify';
-
+import { useNavigate } from "react-router-dom";
 function CreateForm({params}) {
   const form = useContext(FormContext);
   const {id} = useParams();
   const [formData, setFormData] = useState({});
   const [show,setShow] = useState(false);
+  const navigate = useNavigate()
   const valuesToShow = [
     "label",
     "field",
-    "type",
-    "required"
   ]
   useEffect(()=>{
     if(id){
-        setFormData({...formData, "formName":form[id]?.formName})
+        setFormData({"formFields":form[id]?.formFields, "formName":form[id]?.formName})
     }
   },[id]);
 
@@ -60,6 +59,17 @@ function CreateForm({params}) {
     setFormData({...form})
   }
 
+  const onSave = async() =>{
+    let newForm = form ?? {};
+    const splitName = formData.formName?.trim()?.toLowerCase()?.split(" ");
+    const formName = splitName.join("-");
+    newForm[id ?? formName?.toString()] = {
+        ...formData
+    }
+    sessionStorage.setItem("userForms" ,JSON.stringify(newForm))
+    navigate("/");
+  }
+
   return (
     <div>
         <input 
@@ -91,6 +101,14 @@ function CreateForm({params}) {
         }
         <div className='d-flex' style={{marginTop:"20px"}}>
             <div className='input-card' style={{cursor:"pointer"}} onClick={()=>{setShow(true);}}>+ Add Input Field</div>
+        </div>
+        <div style={{display:"flex",justifyContent:"end",marginTop:"40px"}}>
+            <button className='btn btn-secondary' 
+                disabled={(formData?.formFields?.length > 0 && formData?.formName)?false:true}
+                style={{background:(formData?.formFields?.length > 0 && formData?.formName)?"":"rgb(187 190 191)"}}
+                onClick={()=>{onSave()}}
+            >Save
+            </button>
         </div>
         {show !== false && <FieldModal handleModalClose={handleModalClose} data={formData?.formFields?.[show] ?? false} handleSubmit={handleSubmit} formData={formData?.formFields ?? []} />}
     </div>
